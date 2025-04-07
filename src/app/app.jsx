c3c945eec94b7da9ed21a7dc5fadccd4ceeac19a
somin2352 <somin030502@gmail.com>
2025-04-07 18:00:58 +0900

    Init: vite-react-ts scaffold

---
+++ src/app/app.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>React 앱 스캐폴드</h1>
+      <p>Vite 빌드 도구에서 사용 가능한 커스텀 템플릿 작성</p>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,53 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "node:url";
+
+const viteConfig = defineConfig((env) => {
+  const isDevMode = env.mode.includes("development");
+  return {
+    // base: isDevMode ? '/' : '/hobby-dobby/',
+    base: isDevMode ? "/" : "/",
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: [
+              "react",
+              "react-dom",
+              "react-router",
+              "react-error-boundary",
+              "react-textarea-autosize",
+            ],
+            ecosystem: ["zustand", "clsx"],
+            sweetalert: ["sweetalert2"],
+            supabase: ["@supabase/supabase-js"],
+          },
+        },
+      },
+    },
+    plugins: [
+      react({
+        jsxRuntime: "automatic",
+      }),
+    ],
+    publicDir: "public",
+    server: {
+      host: "localhost",
+      port: 3000,
+    },
+    preview: {
+      host: "localhost",
+      port: 8080,
+    },
+    css: {
+      devSourcemap: true,
+    },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
+});
+
+export default viteConfig;
